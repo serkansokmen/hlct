1769812b1c0a5b9f07777d789867af3fe95b1d0b
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2016-03-08 03:36:50 +0200

    seperated image switchers

--- src/hlct/GameAsset.h
+++ src/hlct/GameAsset.h
@@ -5,40 +5,51 @@
 
 
 namespace hlct {
-    class GameAsset {
-        ofImage title0, title1, win, loose;
+    
+    class ImageSwitcher {
+        ofImage img0, img1;
         shared_ptr<ofImage> img;
     public:
-        void setup(){
-            title0.load("game/title_0.png");
-            title1.load("game/title_1.png");
-            win.load("game/win.png");
-            loose.load("game/loose.png");
+        void setup(const string& path0, const string& path1){
+            img0.load(path0);
+            img1.load(path1);
             img = shared_ptr<ofImage>(new ofImage);
+        };
+        void draw(const GameState& state){
+            if (state != GAME_STATE_GAME){
+                int seconds = (int)(ofGetElapsedTimeMillis() / 2000);
+                if (seconds % 2 == 0) {
+                    *img = img0;
+                } else {
+                    *img = img1;
+                }
+                img->draw(0, 0);
+            }
+        };
+    };
+    
+    class GameAsset {
+        ImageSwitcher switchImgTitle, switchImgWin, switchImgLoose;
+        
+    public:
+        void setup(){
+            switchImgTitle.setup("game/title_0.png", "game/title_1.png");
+            switchImgWin.setup("game/win_0.png", "game/win_1.png");
+            switchImgLoose.setup("game/loose_0.png", "game/loose_1.png");
         }
         void draw(const GameState& state){
             switch (state) {
-                case GAME_STATE_TITLE: {
-                    int seconds = (int)(ofGetElapsedTimeMillis() / 2000);
-                    if (seconds % 2 == 0) {
-                        *img = title0;
-                    } else {
-                        *img = title1;
-                    }
+                case GAME_STATE_TITLE:
+                    switchImgTitle.draw(state);
                     break;
-                }
                 case GAME_STATE_END_WIN:
-                    *img = win;
+                    switchImgWin.draw(state);
                     break;
                 case GAME_STATE_END_LOOSE:
-                    *img = loose;
+                    switchImgLoose.draw(state);
                     break;
                 default:
                     break;
-            }
-            
-            if (state != GAME_STATE_GAME){
-                img->draw(0, 0);
             }
         }
     };
